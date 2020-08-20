025edb3dd6453d12c326e8da42c9779393192d1c
Vidushi Haksar <60004269+vidushihaksar@users.noreply.github.com>
2020-08-20 16:22:46 +0530

    first commit

---
+++ binaryTree.java/LC_112_pathSum.java
@@ -0,0 +1,18 @@
+package binaryTree.java;
+
+public class LC_112_pathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        
+        if(root == null ) return false;
+        if(root.left == null && root.right == null && sum-root.val == 0){
+            return true;
+        }
+        
+        boolean res = false;
+        
+        res = res || hasPathSum(root.left, sum - root.val);
+        res = res || hasPathSum(root.right, sum - root.val);
+        
+        return res;
+    }
+}
